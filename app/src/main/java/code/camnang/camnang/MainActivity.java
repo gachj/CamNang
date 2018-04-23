cdb5d8b992b467f6bd6a5773546bd54bfd4fc737
gachj <gachj_bmt@yahoo.com>
2018-04-23 11:30:18 +0700

    Update icon_yoga

--- app/src/main/java/code/camnang/camnang/MainActivity.java
+++ app/src/main/java/code/camnang/camnang/MainActivity.java
@@ -2,12 +2,24 @@ package code.camnang.camnang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgCamnang, imgMonan, imgYoga, imgThongtin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AddControl();
+    }
+
+    private void AddControl() {
+
+        imgCamnang.findViewById(R.id.imgCamnang);
+        imgMonan.findViewById(R.id.imgMonan);
+        imgYoga.findViewById(R.id.imgYoga);
+        imgThongtin.findViewById(R.id.imgThongtin);
     }
 }
