cb074af08f546bbba4d78c56b9c61c75f8f51ec4
gachj <gachj_bmt@yahoo.com>
2018-04-24 12:23:40 +0700

    Intent chuyen activity sang cam nang

---
+++ app/src/main/java/code/camnang/camnang/CamnangActivity.java
@@ -0,0 +1,13 @@
+package code.camnang.camnang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CamnangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camnang);
+    }
+}
